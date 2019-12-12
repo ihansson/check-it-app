aa98e1122321c3da99d917481af80fd13e1d21b0
Ian Hansson <ian.hansson@gmail.com>
2019-12-12 18:40:44 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useContext, Suspense } from 'react';
+import useWindowWidth from './useWindowWidth';
+import { ThemeContext } from './ThemeContext';
+
+export default function App() {
+	const [count, setCount]: [number, (x: number) => void] = useState(5);
+	const width: number = useWindowWidth();
+	const { color, background } = useContext(ThemeContext);
+
+	useEffect(() => {
+		document.title = count;
+	}, [count])
+
+	return <div style={{ color, background }}>
+		<div>
+			Test {count}
+		</div>
+		<div>
+			<button onClick={() => { setCount(count-1) }}>Subtract</button>
+			<button onClick={() => { setCount(count+1) }}>Add</button>
+		</div>
+		Window width: {width}
+	</div>;
+}
