0f7f544f7d32835341e49614c7170b1828120349
Ian Hansson <ian.hansson@gmail.com>
2019-12-15 19:15:18 +0000

    sub routes

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route
+} from "react-router-dom";
 
 const Dashboard: React.FC = () => {
 	return (
 	<div className="App-box">
 		<div className="App-dashboard">
 			<div className="App-dashboard-menu">
-				Menu
+				<nav className="App-dashboard-nav">
+					<a className="active" href="/dashboard">Dashboard</a>
+					<a href="/dashboard/schedule">Schedule</a>
+					<a href="/dashboard/projects">Projects</a>
+					<a href="/dashboard/packages">Packages</a>
+					<a href="/dashboard/settings">Settings</a>
+				</nav>
 			</div>
 			<div className="App-dashboard-messages">
 				Messages
 			</div>
 			<div className="App-dashboard-content">
-				Content
+				<Router>
+					<Switch>
+						<Route path="/dashboard/schedule">
+							Schedule
+						</Route>
+						<Route path="/dashboard">
+							Dashboard
+						</Route>
+					</Switch>
+				</Router>
 			</div>
 		</div>
 	</div>)
